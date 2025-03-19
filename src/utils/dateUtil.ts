14ce81a696e4aebbab4cf443f4efab77faed2752
amaiice <138089174+Ponzu-IKA@users.noreply.github.com>
2025-03-19 14:24:52 +0900

    update: README.md

--- src/utils/dateUtil.ts
+++ src/utils/dateUtil.ts
@@ -1,4 +1,4 @@
-export function date2Timestamp(date: number, type: string = "F"): string {
+export function date2Timestamp(date: number, type = "F"): string {
     const discordTimestamp = Math.round(date/1000)
     return `<t:${discordTimestamp}:${type}>`
 }
