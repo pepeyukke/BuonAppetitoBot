0b14bd5de3ce95648ec7043dfee1acd01f51cc84
hayato <hayato.fukami.0808@gmail.com>
2025-03-22 02:56:13 +0900

    Add SQLite database initialization and table creation
    Introduced logic to create a database directory, initialize SQLite files, and define tables for moderator and bump roles. Refactored bot startup to include the database setup process before logging in the client. Improved error handling and logging for database operations.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,78 @@
 import {SapphireClient} from "@sapphire/framework";
 import {GatewayIntentBits} from "discord.js";
+import path from "node:path";
+import fs from "node:fs";
+import sqlite3 from "sqlite3"
 import {logger} from "./utils/logs";
 
-const client = new SapphireClient({
-    intents: [GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers],
-    loadMessageCommandListeners: true
-});
+const createModeratorRoleTable = `
+CREATE TABLE IF NOT EXISTS moderatorRole (
+    guildId TEXT NOT NULL,
+    roleId TEXT NOT NULL
+);
+`
+const createBumpRoleTable = `
+CREATE TABLE IF NOT EXISTS bumpRole (
+    guildId TEXT NOT NULL,
+    roleId TEXT NOT NULL
+)
+`
 
-const token = process.env.BUON_APPETITO_TOKEN;
+async function start(): Promise<void> {
+    const databaseDirectory = path.join(__dirname, "..", "database");
+    try {
+        if (!fs.existsSync(databaseDirectory)) {
+            fs.mkdirSync(databaseDirectory);
+        }
+    } catch (err) {
+        return logger.error(`Error failed creating directory: "${err}"`);
+    }
 
-if (token) {
-    client.login(token)
-} else {
-    logger.error("No token found")
+    const databaseFiles = ["settings.sqlite"];
+    databaseFiles.forEach((fileName) => {
+        try {
+            const filePath = path.join(databaseDirectory, fileName);
+            if (!fs.existsSync(filePath)) {
+                const db = new sqlite3.Database(filePath);
+                db.close()
+                logger.info(`Created database file: ${filePath}`);
+            } else {
+                logger.info(`Database file already exists: ${filePath}`);
+            }
+        } catch (e) {
+            return logger.error(`Error failed creating database file: "${e}"`);
+        }
+    })
+
+    await createDatabase(path.join(databaseDirectory, "settings.sqlite"), createModeratorRoleTable, "settings.sqlite");
+    await createDatabase(path.join(databaseDirectory, "settings.sqlite"), createBumpRoleTable, "settings.sqlite");
+
+    const token = process.env.BUON_APPETITO_TOKEN;
+    if (!token) {
+        return logger.error("No token found");
+    }
+
+    const client = new SapphireClient({
+        intents: [GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers],
+        loadMessageCommandListeners: true
+    });
+
+    await client.login(token);
+}
+
+start();
+
+async function createDatabase(filePath: string, query: string, dbName: string) {
+    const db = new sqlite3.Database(filePath, (err) => {
+        if (err) {
+            return logger.error(`Error creating database: "${err}"`);
+        }
+    });
+    db.run(query, (err) => {
+        if (err) {
+            return logger.error(`Error creating database: "${err}"`);
+        } else {
+            return logger.info(`Created database: ${dbName}`);
+        }
+    });
 }
