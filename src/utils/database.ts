76ac772237c505cd810504fb307874e42c76a0bd
hayato <hayato.fukami.0808@gmail.com>
2025-03-22 18:57:15 +0900

    Add utility functions for database queries using SQLite
    Introduce `executeGetQuery` and `executeRunQuery` to interact with SQLite databases. These functions handle database connections, execute queries, and log errors for improved debugging.

---
+++ src/utils/database.ts
@@ -0,0 +1,45 @@
+import sqlite3 from "sqlite3";
+import {logger} from "./logs";
+
+async function executeGetQuery(dbPath: string, query: string, params: any[] = []): Promise<any | undefined> {
+    return new Promise<any | undefined>((resolve, reject) => {
+        const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READONLY, (err) => {
+            if (err) {
+                logger.error(`Error failed connect database: "${err}"`);
+                reject(err);
+                return;
+            }
+            db.get(query, params, (err, row) => {
+                if (err) {
+                    logger.error(`Error failed execute query: "${err}"`);
+                    reject(err);
+                } else {
+                    resolve(row);
+                }
+                db.close();
+            });
+        });
+    });
+}
+
+
+async function executeRunQuery(dbPath: string, query: string, params: any[] = []): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
+            if (err) {
+                reject(err);
+                logger.error(`Error failed connect database: "${err}"`);
+                return;
+            }
+            db.run(query, params, (err) => {
+                if (err) {
+                    logger.error(`Error failed execute query: "${err}"`);
+                    reject(err);
+                } else {
+                    resolve();
+                }
+                db.close();
+            });
+        });
+    });
+}
