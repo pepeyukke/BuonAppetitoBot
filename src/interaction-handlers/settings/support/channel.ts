157700672eb54ee2c3f9673471fec901815d2652
hayato <hayato.fukami.0808@gmail.com>
2025-03-26 22:52:16 +0900

    Add support role verification to support channel creation
    This change ensures that a support role is configured before creating a support channel. If the role is not found, an appropriate message is sent to the user, and the process is safely terminated. Additionally, error handling has been improved to provide clear feedback in case of unexpected issues.

--- src/interaction-handlers/settings/support/channel.ts
+++ src/interaction-handlers/settings/support/channel.ts
@@ -2,12 +2,35 @@ import { ApplyOptions } from '@sapphire/decorators';
 import { InteractionHandler, InteractionHandlerTypes } from '@sapphire/framework';
 import {ActionRowBuilder, ButtonInteraction, ChannelSelectMenuBuilder, ChannelType, MessageFlags} from 'discord.js';
 import {logger} from "../../../utils/logs";
+import {executeGetQuery} from "../../../utils/database";
 
 @ApplyOptions<InteractionHandler.Options>({
 	interactionHandlerType: InteractionHandlerTypes.Button
 })
 export class ButtonHandler extends InteractionHandler {
 	public async run(interaction: ButtonInteraction) {
+		const roleIdQuery = `
+			SELECT roleId as id
+			FROM supportRole
+			WHERE guildId = ?;
+		`
+		try {
+			const row = await executeGetQuery("../database/support.sqlite", roleIdQuery, [interaction.guildId]);
+			if (!row.id) {
+				await interaction.reply({
+					content: "サポートロールを設定してから再度実行してください。",
+					flags: [MessageFlags.Ephemeral]
+				})
+				return logger.warn(`Failed to create support channel. Support role not found. by ${interaction.user.tag}.`)
+			}
+		} catch (e) {
+			await interaction.reply({
+				content: "エラーが発生しました。管理者に問い合わせてください。",
+				flags: [MessageFlags.Ephemeral]
+			})
+			return logger.error(`Error occurred while creating support channel: ${e}`)
+		}
+
 		const select = new ChannelSelectMenuBuilder()
 			.setCustomId("settings:support:channel:select")
 			.setPlaceholder("サポートチャンネルを選択してください。")
