352c61829bc10c48e645efd16bcc490427bfa685
hayato <hayato.fukami.0808@gmail.com>
2025-03-29 11:43:29 +0900

    Add setup script for database initialization
    This script creates necessary directories, initializes SQLite databases, and executes SQL scripts to set up required tables. It ensures the environment is ready for application operations and logs progress or errors accordingly.

---
+++ src/setup.ts
@@ -0,0 +1,60 @@
+import fs from "node:fs";
+import {logger} from "./utils/logs";
+import path from "node:path";
+import sqlite3 from "sqlite3";
+import {readFile} from "./utils/readFile";
+import {executeRunQuery} from "./utils/database";
+
+export async function setup() {
+    const databaseDirectory = path.join(__dirname, "../database");
+    try {
+        if (!fs.existsSync(databaseDirectory)) {
+            fs.mkdirSync(databaseDirectory);
+            logger.info(`Created directory: ${databaseDirectory}`);
+        } else {
+            logger.info(`Directory already exists: ${databaseDirectory}`);
+        }
+    } catch (err) {
+        return logger.error(`Error failed creating directory: "${err}"`);
+    }
+
+    const databaseFiles = ["general.sqlite", "support.sqlite"];
+    for (const fileName of databaseFiles) {
+        const filePath = path.join(databaseDirectory, fileName)
+        if (!fs.existsSync(filePath)) {
+            const db = new sqlite3.Database(filePath, (err) => {
+                if (err) {
+                    return logger.error(`Error failed connect database: "${err}"`);
+                }
+            });
+            db.close();
+            logger.info(`Created database: ${filePath}`);
+        } else {
+            logger.info(`Database already exists: ${filePath}`);
+        }
+    }
+
+    const generalDatabasePath = path.join(databaseDirectory, "general.sqlite");
+    const supportDatabasePath = path.join(databaseDirectory, "support.sqlite");
+    const sqlDirectory = path.join(__dirname, "../sql");
+
+    const createModeratorRoleTable = await readFile(path.join(sqlDirectory, "createModeratorRoleTable.sql"));
+    const createBumpRoleTable = await readFile(path.join(sqlDirectory, "createBumpRoleTable.sql"));
+    const createSupportRoleTable = await readFile(path.join(sqlDirectory, "createSupportRoleTable.sql"));
+    const createSupportChannelTable = await readFile(path.join(sqlDirectory, "createSupportChannelTable.sql"));
+
+    if (createModeratorRoleTable) {
+        await executeRunQuery(generalDatabasePath, createModeratorRoleTable);
+    }
+    if (createBumpRoleTable) {
+        await executeRunQuery(generalDatabasePath, createBumpRoleTable);
+    }
+    if (createSupportRoleTable) {
+        await executeRunQuery(supportDatabasePath, createSupportRoleTable);
+    }
+    if (createSupportChannelTable) {
+        await executeRunQuery(supportDatabasePath, createSupportChannelTable);
+    }
+
+    return logger.info("Setup completed");
+}
